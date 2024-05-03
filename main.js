9fc4e8018d2e03ed6194c861aa9e14991fe6ab31
allanadasilvapego <127985302+allanadasilvapego@users.noreply.github.com>
2024-05-02 21:27:01 -0300

    Update main.js

--- main.js
+++ main.js
@@ -8,14 +8,13 @@ for(let i=0; i<botoes.length; i++){
         botoes[i].classList.add("ativo");
     }
 }
-const abas
-    = document.querySelectorAll(".aba-conteudo");
+const botoes = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0; i<abas.length; i++){
-    abas[i].onclick = function(){
-        for(j=0; j<abas.length;j++){
-            abas[j].classList.remove("ativo");
+for(let i=0; i<botoes.length; i++){
+    botoes[i].onclick = function(){
+        for(j=0; j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
         }
-       abas[i].classList.add("ativo");
+        botoes[i].classList.add("ativo");
     }
 }
